96a940da9001cb3d0e2864c2e9c9a439b13ade52
viniciusgandrade <viniciusgandrade@gmail.com>
2015-12-22 10:55:13 -0200

    Update MbaRestProxy.js
    Mudança para permitir parâmetros na url.

--- MbaRestProxy.js
+++ MbaRestProxy.js
@@ -5,6 +5,21 @@ Ext.define('Mba.ux.Data.MbaRestProxy', {
 
     buildUrl: function (request) {
         this.callParent([request]);
+
+        var params    = request.getParams() || {},
+            url       = this.getUrl(request),
+            replaceStr = '';
+
+        for (var property in params) {
+            replaceStr = '{' + property + '}';
+            if (url.indexOf(replaceStr) != -1) {
+                url = url.replace(replaceStr, params[property]);
+                delete params[property];
+            }
+        }
+
+        request.setUrl(url);
+
         return this.getModel().prototype.buildRestUrl(request);
     }
 
